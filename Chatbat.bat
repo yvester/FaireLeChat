18ea245d27e70cc240f098cff93767086ae53c14
yvester <yves.l.tanguy@gmail.com>
2017-11-10 14:44:29 +0100

    Delete Chatbat.bat

--- Chatbat.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Welcome to FaireLeChat! Date : %DATE% Time : %TIME% Name : %1 > temp.txt
-REM C:\\Users\\ytanguy\\Downloads\\AAA\\testmd5sum\\fciv.exe -wp -md5 %1 >> temp.txt
-"C:\\Program Files (x86)\\FaireLeChat\\fciv.exe" -wp -md5 %1 >> temp.txt
-REM set /p VAR=<temp.txt
-REM set location="bob"
-REM echo|set /p=%1|clip
-REM pause
