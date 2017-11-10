23b17a2c0fc5a82922332a266f40c36ba6dac4b1
yvester <yves.l.tanguy@gmail.com>
2017-11-10 16:23:33 +0100

    Add files via upload

--- source/Chatbat.bat
+++ source/Chatbat.bat
@@ -1,7 +1,7 @@
 @echo off
-echo Welcome to FaireLeChat ou/or SeeTheSha! Date : %DATE% Time : %TIME% Name : %1 > temp.txt
-REM C:\\Users\\ytanguy\\Downloads\\AAA\\testmd5sum\\fciv.exe -wp -md5 %1 >> temp.txt
-"%ProgramFiles(x86)%\FaireLeChat\\fciv.exe" -wp -md5 %1 >> temp.txt
+echo Welcome to FaireLeChat ou/or SeeTheSha! Date : %DATE% Time : %TIME% Name : %1 > %~n1.sha
+"%ProgramFiles(x86)%\FaireLeChat\\fciv.exe" -wp -md5 %1 >> %~n1.sha   
+REM C:\\Users\\ytanguy\\Downloads\\AAA\\testmd5sum\\fciv.exe -wp -md5 %1 >> %~n1.sha
 REM set /p VAR=<temp.txt
 REM set location="bob"
 REM echo|set /p=%1|clip
