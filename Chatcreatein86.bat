7e44d543441dbe19c7603303c9ee1a7779e4f4e5
yvester <yves.l.tanguy@gmail.com>
2017-11-10 11:54:56 +0100

    Add files via upload
    Minor change

--- Chatcreatein86.bat
+++ Chatcreatein86.bat
@@ -1,5 +1,8 @@
 @echo off
+
 echo Welcome to create! Date : %DATE% Time : %TIME% > temp.txt
+set savedir=%cd%
+echo xxxcx %savedir%
 chdir C:\Program Files (x86)\
 REM RMDIR /S /Q [drive:]path RD [/S] [/Q] [drive:]path
 del /s /q C:\Program Files (x86)\FaireLeChat\*
@@ -7,9 +10,13 @@ RMDIR /S /Q C:\Program Files (x86)\FaireLeChat
 mkdir FaireLeChat
 chdir FaireLeChat
 REM copy nul > file.txt
-xcopy /y C:\Users\ytanguy\Downloads\FaireLeChat\Chatbat.bat  "C:\Program Files (x86)\FaireLeChat"
-xcopy /y C:\Users\ytanguy\Downloads\FaireLeChat\ChatReg.bat  "C:\Program Files (x86)\FaireLeChat"
-xcopy /y C:\Users\ytanguy\Downloads\FaireLeChat\fciv.exe  "C:\Program Files (x86)\FaireLeChat"
+echo %HOMEPATH%
+xcopy /y %savedir%\Chatbat.bat  "C:\Program Files (x86)\FaireLeChat"
+xcopy /y %savedir%\ChatReg.bat  "C:\Program Files (x86)\FaireLeChat"
+xcopy /y %savedir%\fciv.exe  "C:\Program Files (x86)\FaireLeChat"
+REM xcopy /y C:\Users\ytanguy\Downloads\FaireLeChat\Chatbat.bat  "C:\Program Files (x86)\FaireLeChat"
+REM xcopy /y C:\Users\ytanguy\Downloads\FaireLeChat\ChatReg.bat  "C:\Program Files (x86)\FaireLeChat"
+REM xcopy /y C:\Users\ytanguy\Downloads\FaireLeChat\fciv.exe  "C:\Program Files (x86)\FaireLeChat"
 REM xcopy /s c:\source d:\target
 REM C:\\Users\\ytanguy\\Downloads\\AAA\\testmd5sum\\fciv.exe -wp -md5 %1 >> temp.txt
 REM set /p VAR=<temp.txt
