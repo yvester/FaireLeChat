81c1b978af403c5fc5a1cb59026b43453b0911ca
yvester <yves.l.tanguy@gmail.com>
2017-11-10 09:26:19 +0100

    Initial Commit

---
+++ Chatcreatein86.bat
@@ -0,0 +1,18 @@
+@echo off
+echo Welcome to create! Date : %DATE% Time : %TIME% > temp.txt
+chdir C:\Program Files (x86)\
+REM RMDIR /S /Q [drive:]path RD [/S] [/Q] [drive:]path
+del /s /q C:\Program Files (x86)\FaireLeChat\*
+RMDIR /S /Q C:\Program Files (x86)\FaireLeChat
+mkdir FaireLeChat
+chdir FaireLeChat
+REM copy nul > file.txt
+xcopy /y C:\Users\ytanguy\Downloads\FaireLeChat\Chatbat.bat  "C:\Program Files (x86)\FaireLeChat"
+xcopy /y C:\Users\ytanguy\Downloads\FaireLeChat\ChatReg.bat  "C:\Program Files (x86)\FaireLeChat"
+xcopy /y C:\Users\ytanguy\Downloads\FaireLeChat\fciv.exe  "C:\Program Files (x86)\FaireLeChat"
+REM xcopy /s c:\source d:\target
+REM C:\\Users\\ytanguy\\Downloads\\AAA\\testmd5sum\\fciv.exe -wp -md5 %1 >> temp.txt
+REM set /p VAR=<temp.txt
+REM set location="bob"
+REM echo|set /p=%1|clip
+REM pause
