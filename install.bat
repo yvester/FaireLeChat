10dfad789e6cc5f45fa4443302e23573a9363459
yvester <yves.l.tanguy@gmail.com>
2017-11-10 14:42:34 +0100

    Add files via upload
    Adding new install file

---
+++ install.bat
@@ -0,0 +1,26 @@
+@echo off
+echo Welcome to create! Date : %DATE% Time : %TIME% 
+chdir %ProgramFiles(x86)%
+del /s /q %ProgramFiles(x86)%\FaireLeChat\*
+RMDIR /S /Q %ProgramFiles(x86)%\FaireLeChat
+mkdir FaireLeChat
+chdir FaireLeChat
+set NamePath=%~p0
+xcopy /y C:%NamePath%Chatbat.bat "%ProgramFiles(x86)%\FaireLeChat"
+xcopy /y C:%NamePath%ChatReg.bat "%ProgramFiles(x86)%\FaireLeChat"
+xcopy /y C:%NamePath%fciv.exe "%ProgramFiles(x86)%\FaireLeChat"
+
+REM Execute the registry update
+C:%NamePath%ChatReg.bat "%ProgramFiles(x86)%\FaireLeChat"
+
+REM C:\\Users\\ytanguy\\Downloads\\AAA\\testmd5sum\\fciv.exe -wp -md5 %1 >> temp.txt
+REM set /p VAR=<temp.txt
+REM set location="bob"
+REM echo|set /p=%1|clip
+REM set savedir=%cd%
+REM echo xxxcx %savedir%
+REM %ProgramFiles(x86)%
+REM echo %HOMEPATH%
+REM echo program %0
+REM echo %~p0
+pause
