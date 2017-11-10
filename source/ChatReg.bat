32b5ad28a45c7391ce2a9f8b9dd90aa7cb47d263
yvester <yves.l.tanguy@gmail.com>
2017-11-10 15:46:10 +0100

    Add files via upload

--- source/ChatReg.bat
+++ source/ChatReg.bat
@@ -1,9 +1,11 @@
 @echo off
-echo Welcome to FaireLeChat§ pour la branche1!
+echo Set the registry key for FaireLeChat!
+reg add HKEY_CLASSES_ROOT\7-Zip.zip\shell\FaireLeChat\command /t REG_SZ /d "%ProgramFiles(x86)%\FaireLeChat\Chatbat.bat %%1" /f
+
+REM reg add HKEY_CLASSES_ROOT\7-Zip.zip\shell\FaireLeChat\command /t REG_SZ /d "C:\Program Files (x86)\FaireLeChat\Chatbat.bat %%1" /f
 REM reg add HKEY_CLASSES_ROOT\7-Zip.zip\shell\FaireLeChat\command /t REG_SZ /d "C:\\Users\\ytanguy\\Documents\\documentation\\\\Testbat.bat %%1"     <<<<< WORKS
 REM reg add HKEY_CLASSES_ROOT\7-Zip.zip\shell\FaireLeChat\command /t REG_SZ /d "C:\Program Files (x86)\FaireLeChat\Testbat.bat %1"  /f
 REM reg add HKEY_CLASSES_ROOT\7-Zip.zip\shell\FaireLeChat\command /t REG_SZ /d "C:\Program Files (x86)\FaireLeChat\Testbat.bat" "%1"  /f
-reg add HKEY_CLASSES_ROOT\7-Zip.zip\shell\FaireLeChat\command /t REG_SZ /d "C:\Program Files (x86)\FaireLeChat\Chatbat.bat %%1" /f
 REM "C:\Program Files (x86)\FaireLeChat\Testbat.bat" "%1"    <------- marche 
 REM C:\\Users\\ytanguy\\Documents\\documentation\\\\Testbat.bat %1 <------- marche
 REM C:\\Users\\ytanguy\\Downloads\\AAA\\testmd5sum\\fciv.exe -wp -md5 %1 > temp.txt
